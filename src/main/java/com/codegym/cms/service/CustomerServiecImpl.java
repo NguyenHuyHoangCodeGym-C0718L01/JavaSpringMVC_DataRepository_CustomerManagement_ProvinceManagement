22301938f5424389008ea39b58c43024f52f8c8b
hoangnhb <hoangnhb@bkav.com>
2019-02-28 10:04:16 +0700

    Hoang finish Java Spring MVC Data Repository Customer Management And Province Management project.

---
+++ src/main/java/com/codegym/cms/service/CustomerServiecImpl.java
@@ -0,0 +1,42 @@
+package com.codegym.cms.service;
+import com.codegym.cms.model.Customer;
+import com.codegym.cms.model.Province;
+import com.codegym.cms.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class CustomerServiecImpl implements CustomerService{
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public Iterable<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findOne(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.delete(id);
+    }
+
+    @Override
+    public Iterable<Customer> findAllByProvince(Province province) {
+        return customerRepository.findAllByProvince(province);
+    }
+}
